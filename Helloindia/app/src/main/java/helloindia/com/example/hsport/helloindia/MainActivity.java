e6f0d3c90c91643a5dfaa408f10002d3e50d2458
santuseera123 <santu.seera123@gmail.com>
2017-01-27 18:53:00 +0530

    Add files via upload

---
+++ Helloindia/app/src/main/java/helloindia/com/example/hsport/helloindia/MainActivity.java
@@ -0,0 +1,36 @@
+package helloindia.com.example.hsport.helloindia;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText pass_word;
+    private Button button_sbm;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+
+    public void addListener OnButton() {
+        pass_word = (EditText) findViewById(R.id.editText);
+        button_sbm = (Button) findViewById(R.id.button);
+        button_sbm.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(MainActivity.this, pass_word.getText(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
+    }
+}
+
+
